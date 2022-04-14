4d25541a61bff74d17acedf6ed2ec42cc85a219b
Sahil Jain <sj2394108@gmail.com>
2022-04-15 03:04:00 +0530

    All Done project 90% complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
-import NewsCards from './components/NewsCards/NewsCards';
 import useStyle from './style'
+import NewsCards from './components/NewsCards/NewsCards';
 import wordsToNumbers from 'words-to-numbers';
-import { NewsCards, Modal } from './components';
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
@@ -21,14 +20,14 @@ const App = () => {
           else if (command === 'highlight') {
             setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
           }
-          else if (command === 'instructions') {
-            setIsOpen(true);
-          }
+          // else if (command === 'instructions') {
+          //   setIsOpen(true);
+          // }
           else if (command === 'open') {
             const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
             const article = articles[parsedNumber - 1];
 
-            if (parsedNumber > articles.length) {
+            if (parsedNumber > 20) {
               alanBtn().playText('Please try that again...');
             } else if (article) {
               window.open(article.url, '_blank');
